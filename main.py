caf8ea928160f945a9a6060a2aa2564de4111936
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-02-21 21:00:49 -0300

    Create file client.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-from os import getenv
-from asyncio import run
+from pyrogram import filters
 
-from dotenv import load_dotenv
-from pyrogram import Client, filters
+from client import create_app
 
-load_dotenv()
-
-app = Client(
-    'guibot',
-    api_id=getenv('TELEGRAM_API_ID'),
-    api_hash=getenv('TELEGRAM_API_HASH'),
-    bot_token=getenv('TELEGRAM_BOT_TOKEN')
-)
+app = create_app()
 
 print('🤖 Bot started')
 
